5ccfe0f04dc418b9ee4ba372664227e8e1898c4a
Flavio <flaviosantana.c@gmail.com>
2021-02-18 15:10:47 -0300

    feat: adicionado life cycle hooks

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnChanges, OnInit, AfterViewInit, OnDestroy {
 
   @Input() title = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    console.log('CONSTRUTOR ');
   }
 
+  ngOnChanges(): void {
+    console.log('ngOnChanges');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy(): void {
+    console.log('Fui destruído')
+  }
 }
