127da60c10889d1c935c170e628f62648ef08d24
Flavio <flaviosantana.c@gmail.com>
2021-02-19 01:15:15 -0300

    feat: adicionado módulo service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,17 @@
 import '@angular/common/locales/global/fr';
 
 import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import localePt from '@angular/common/locales/pt';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SharedModule } from '../app/shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ExercicioContadorComponent } from './shared/components/contador/exercicio-contador.component';
-import { ExercicioDataBindingComponent } from './shared/components/exercicio-data-binding/exercicio-data-binding.component';
-import { ExercicioDiretivasComponent } from './shared/components/exercicio-diretivas/exercicio-diretivas.component';
-import {
-  ExercicioMemesAgrupadosComponent,
-} from './shared/components/exercicio-memes-agrupados/exercicio-memes-agrupados.component';
-import { ExercicioNgclassComponent } from './shared/components/exercicio-ngclass/exercicio-ngclass.component';
-import { ExercicioPipesComponent } from './shared/components/exercicio-pipes/exercicio-pipes.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { LoginComponent } from './login/login.component';
 import { ExtratoComponent } from './extrato/extrato.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 
 registerLocaleData(localePt, 'pt');
@@ -27,22 +19,16 @@ registerLocaleData(localePt, 'pt');
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     HomeComponent,
-    FooterComponent,
-    ExercicioDataBindingComponent,
-    ExercicioContadorComponent,
-    ExercicioDiretivasComponent,
-    ExercicioMemesAgrupadosComponent,
-    ExercicioNgclassComponent,
-    ExercicioPipesComponent,
     LoginComponent,
     ExtratoComponent,
     NaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule,
   ],
   providers: [{
     provide: LOCALE_ID,
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { ExercicioContadorComponent } from './components/contador/exercicio-contador.component';
+import { ExercicioDataBindingComponent } from './components/exercicio-data-binding/exercicio-data-binding.component';
+import { ExercicioDiretivasComponent } from './components/exercicio-diretivas/exercicio-diretivas.component';
+import {
+  ExercicioMemesAgrupadosComponent,
+} from './components/exercicio-memes-agrupados/exercicio-memes-agrupados.component';
+import { ExercicioNgclassComponent } from './components/exercicio-ngclass/exercicio-ngclass.component';
+import { ExercicioPipesComponent } from './components/exercicio-pipes/exercicio-pipes.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+
+
+@NgModule({
+  declarations: [
+    ExercicioDataBindingComponent,
+    ExercicioContadorComponent,
+    ExercicioDiretivasComponent,
+    ExercicioMemesAgrupadosComponent,
+    ExercicioNgclassComponent,
+    ExercicioPipesComponent,
+    FooterComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
+  exports: [
+    ExercicioDataBindingComponent,
+    ExercicioContadorComponent,
+    ExercicioDiretivasComponent,
+    ExercicioMemesAgrupadosComponent,
+    ExercicioNgclassComponent,
+    ExercicioPipesComponent,
+    FooterComponent,
+    HeaderComponent,
+  ]
+})
+export class SharedModule { }
